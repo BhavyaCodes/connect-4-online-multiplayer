c36c5b83033d8abda9cb27e233258803441fd39d
Juggernaut9 <58269749+Juggernaut9@users.noreply.github.com>
2021-01-28 06:12:56 +0530

    finish room creation (#4)

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const rooms = {};
+
+app.use(express.json());
+app.get("/api/rooms", (req, res) => {
+  res.json(rooms);
+});
+
+app.post("/api/room", (req, res) => {
+  rooms[req.body.id] = { 0: req.body };
+  io.emit("room-created", rooms);
+  res.json();
+});
+
 app.get("/api/ping", (req, res, next) => {
   res.json({ ping: "pong" });
 });
 
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+  socket.on("new-room", (room, user) => {});
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
